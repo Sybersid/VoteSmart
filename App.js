650c839592d73fe649d3eb92f2fd4ecf5bcce17f
Sybersid <sidhdharth.suresh@gmail.com>
2019-10-04 17:55:59 -0500

    voteSmart

---
+++ App.js
@@ -0,0 +1,59 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+import Icon from 'react-native-vector-icons/Ionicons';
+import { createAppContainer } from 'react-navigation';
+import { createBottomTabNavigator } from 'react-navigation-tabs'; 
+import CanidateScreen from './Canidates'
+import TopicScreen from './Topics'
+import VoteScreen from './Vote'
+import NewsScreen from './News'
+
+
+const TabNavigator = createBottomTabNavigator({
+  Canidates: { screen: CanidateScreen,
+    navigationOptions: {
+      tabBarLabel: 'Canidates',
+      tabBarIcon: ({tintColor}) => (
+        <Icon name="ios-contact" color={tintColor} size={24} />
+      )
+    }
+  },
+  News: { screen: NewsScreen,
+    navigationOptions: {
+      tabBarLabel: 'News',
+      tabBarIcon: ({tintColor}) => (
+        <Icon name="ios-list" color={tintColor} size={24} />
+      )
+    }
+  },
+  Topics:  { screen: TopicScreen,
+    navigationOptions: {
+      tabBarLabel: 'Topics',
+      tabBarIcon: ({tintColor}) => (
+        <Icon name="ios-search" color={tintColor} size={24} />
+      )
+    }
+  },
+  Vote:  { screen: VoteScreen,
+    navigationOptions: {
+      tabBarLabel: 'Vote',
+      tabBarIcon: ({tintColor}) => (
+        <Icon name="ios-information-circle" color={tintColor} size={24} />
+      )
+    }
+  }
+  },
+{
+  initialRouteName : 'Vote',
+  order: ['Canidates', 'Topics', 'News', 'Vote'],
+
+  navigationOptions: {
+    tabBarVisible: true
+  },
+  tabBarOptions: {
+    activeTintColor: 'crimson',
+    inactiveTintColor: 'dodgerblue'
+  }
+});
+
+export default createAppContainer(TabNavigator);
---
+++ Canidates.js
@@ -0,0 +1,64 @@
+import React, { Component } from "react";
+import {
+    View,
+    Text,
+    StyleSheet,
+    SafeAreaView,
+    TextInput,
+    Platform,
+    StatusBar,
+    ScrollView,
+    Image,
+    Dimensions
+} from "react-native";
+
+const { height, width } = Dimensions.get('window')
+
+class CanidatesScreen extends Component {
+
+    componentWillMount() {
+        this.startHeaderHeight = 80
+        if (Platform.OS == 'android') {
+            this.startHeaderHeight = 100 + StatusBar.currentHeight
+        }
+    }
+
+    render() {
+        return (
+            <SafeAreaView style={{ flex: 1 }}>
+                    <ScrollView
+                        scrollEventThrottle={16}
+                    >
+                        <View style={{ flex: 1, backgroundColor: 'white', paddingTop: 20, justifyContent: "center" }}>
+                            <Text style={{ fontSize: 32, fontWeight: '800', paddingHorizontal: 20, color:'blue' }}>
+                                Canidates
+                            </Text>
+                            <Text style={{ fontSize: 20, fontWeight: '300', paddingHorizontal: 20, color:'red' }}>
+                                Make an informed voting decision by learning about each canidates.</Text>
+                        </View>
+                        <View>
+                            
+                        </View>
+                </ScrollView>
+            </SafeAreaView>
+        );
+    }
+}
+
+export default CanidatesScreen;
+
+const styles = StyleSheet.create({
+  container: {
+
+  },
+  header: {
+      paddingRight: 15,
+      paddingLeft: 15
+  },
+  content: {
+      display: "flex",
+      flex: 1,
+      justifyContent: "center",
+      padding: 15
+  }
+});
