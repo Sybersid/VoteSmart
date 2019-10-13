7baf3cbaace679b95cb57e2cbdaf706581999dac
Michael Gathara <mikegtrm@gmail.com>
2019-10-13 02:31:24 -0500

    Added fetching of news data

--- News.js
+++ News.js
@@ -11,10 +11,37 @@ import {
     Image,
     Dimensions
 } from "react-native";
+import { FlatList } from "react-native-gesture-handler";
+
+/*
+Quick to do
+    Get News Api and use Json data to populate news
+    API key:
+    a1215f14d6e3425d9c12e87fbd59bed1
+*/
+
 
 const { height, width } = Dimensions.get('window')
 
 class NewsScreen extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            loading: true,
+            dataSource:[]
+        };
+    }
+    componentDidMount(){
+        fetch("https://newsapi.org/v2/top-headlines?country=us&apiKey=a1215f14d6e3425d9c12e87fbd59bed1")
+        .then(response => response.json())
+        .then((responseJson)=> {
+          this.setState({
+           loading: false,
+           dataSource: responseJson
+          })
+        })
+        .catch(error=>console.log(error)) //If errors do occur. They should be caught here
+    }
 
     componentWillMount() {
         this.startHeaderHeight = 80
@@ -24,24 +51,41 @@ class NewsScreen extends Component {
     }
 
     render() {
-        return (
-            <SafeAreaView style={{ flex: 1 }}>
-                    <ScrollView
-                        scrollEventThrottle={16}
-                    >
-                        <View style={{ flex: 1, backgroundColor: 'white', paddingTop: 20, justifyContent: "center" }}>
-                            <Text style={{ fontSize: 32, fontWeight: '800', paddingHorizontal: 20, color:'blue' }}>
-                                News
-                            </Text>
-                            <Text style={{ fontSize: 20, fontWeight: '300', paddingHorizontal: 20, color:'red' }}>
-                                Get the latest news on each canidate.</Text>
-                        </View>
-                        <View>
+        if(this.state.loading) {
+            return (
+                <View style={styles.content}>
+                    <ActivityIndicator size="large" color="#0c9"/>
+                </View>
+            )
+        }
+        return(
+            <View style={styles.container}>
+                <FlatList
+                    data = {this.state.dataSource}
+                    ItemSeparatorComponent = {this.FlatListItemSeparator}
+                    renderItem = {item=> this.renderItem(item)}
+                    keyExtractor = {item=>item.id.toString()}
+                />
+            </View>
+        )
+        // return (
+        //     <SafeAreaView style={{ flex: 1 }}>
+        //             <ScrollView
+        //                 scrollEventThrottle={16}
+        //             >
+        //                 <View style={{ flex: 1, backgroundColor: 'white', paddingTop: 20, justifyContent: "center" }}>
+        //                     <Text style={{ fontSize: 32, fontWeight: '800', paddingHorizontal: 20, color:'blue' }}>
+        //                         News
+        //                     </Text>
+        //                     <Text style={{ fontSize: 20, fontWeight: '300', paddingHorizontal: 20, color:'red' }}>
+        //                         Get the latest news on each canidate.</Text>
+        //                 </View>
+        //                 <View>
                             
-                        </View>
-                </ScrollView>
-            </SafeAreaView>
-        );
+        //                 </View>
+        //         </ScrollView>
+        //     </SafeAreaView>
+        // );
     }
 }
 
