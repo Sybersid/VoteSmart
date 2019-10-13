75a9cac612a7f72a094e436bf5bca6756234d7af
Sybersid <sidhdharth.suresh@gmail.com>
2019-10-13 00:17:14 -0500

    downloaded RN elements and added card

--- Canidates.js
+++ Canidates.js
@@ -1,16 +1,6 @@
 import React, { Component } from "react";
-import {
-    View,
-    Text,
-    StyleSheet,
-    SafeAreaView,
-    TextInput,
-    Platform,
-    StatusBar,
-    ScrollView,
-    Image,
-    Dimensions
-} from "react-native";
+import { View, Text, Image, StyleSheet, SafeAreaView, Platform, StatusBar, ScrollView, Dimensions } from 'react-native'
+import { Card, ListItem, Button, Icon } from 'react-native-elements'
 
 const { height, width } = Dimensions.get('window')
 
@@ -27,17 +17,23 @@ class CanidatesScreen extends Component {
         return (
             <SafeAreaView style={{ flex: 1 }}>
                     <ScrollView
-                        scrollEventThrottle={16}
-                    >
-                        <View style={{ flex: 1, backgroundColor: 'white', paddingTop: 20, justifyContent: "center" }}>
-                            <Text style={{ fontSize: 32, fontWeight: '800', paddingHorizontal: 20, color:'blue' }}>
-                                Canidates
-                            </Text>
-                            <Text style={{ fontSize: 20, fontWeight: '300', paddingHorizontal: 20, color:'red' }}>
-                                Make an informed voting decision by learning about each canidates.</Text>
+                        scrollEventThrottle={16}>
+                    <View style={{ flex: 1, backgroundColor: 'white', paddingTop: 20, justifyContent: "center" }}>
+                            <Text style={{ fontSize: 32, fontWeight: '800', paddingHorizontal: 20, color:'blue' }}>Canidates</Text>
+                            <Text style={{ fontSize: 20, fontWeight: '300', paddingHorizontal: 20, color:'red' }}>Make an informed voting decision by learning about each canidates.</Text>
                         </View>
                         <View>
-                            
+                        <Card
+  title='HELLO WORLD'
+  image={require('./assets/icon.png')}>
+  <Text style={{marginBottom: 10}}>
+    The idea with React Native Elements is more about component structure than actual design.
+  </Text>
+  <Button
+    icon={<Icon name='code' color='#ffffff' />}
+    buttonStyle={{borderRadius: 0, marginLeft: 0, marginRight: 0, marginBottom: 0}}
+    title='VIEW NOW' />
+</Card>
                         </View>
                 </ScrollView>
             </SafeAreaView>
