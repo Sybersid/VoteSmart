f0950aab8f7ec91d8bde667b0261588625e5147f
Sybersid <sidhdharth.suresh@gmail.com>
2019-10-19 18:57:57 -0500

    sanders, biden, and trump added to canidates tab

--- Canidates.js
+++ Canidates.js
@@ -3,6 +3,7 @@ import { View, Text, Image, StyleSheet, SafeAreaView, Platform, StatusBar, Scrol
 import { Card, ListItem, Button, Icon } from 'react-native-elements'
 
 const { height, width } = Dimensions.get('window')
+  
 
 class CanidatesScreen extends Component {
 
@@ -22,19 +23,35 @@ class CanidatesScreen extends Component {
                             <Text style={{ fontSize: 32, fontWeight: '800', paddingHorizontal: 20, color:'blue' }}>Canidates</Text>
                             <Text style={{ fontSize: 20, fontWeight: '300', paddingHorizontal: 20, color:'red' }}>Make an informed voting decision by learning about each canidates.</Text>
                         </View>
-                        <View>
-                        <Card
-  title='HELLO WORLD'
-  image={require('./assets/icon.png')}>
-  <Text style={{marginBottom: 10}}>
-    The idea with React Native Elements is more about component structure than actual design.
-  </Text>
+<View>
+        <Text style={{ fontSize: 40, fontWeight: '300', paddingHorizontal: 95, color:'blue' }}> Democrats </Text>
+<Card
+  title='Bernie Sanders'
+  image={require('../voteSmart/images/bsanders.jpg')}>
   <Button
-    icon={<Icon name='code' color='#ffffff' />}
     buttonStyle={{borderRadius: 0, marginLeft: 0, marginRight: 0, marginBottom: 0}}
-    title='VIEW NOW' />
+    title='MORE INFO' />
 </Card>
-                        </View>
+
+<Card
+  title='Joe Biden'
+  image={require('../voteSmart/images/jbiden.jpeg')}>
+  <Button
+    buttonStyle={{borderRadius: 0, marginLeft: 0, marginRight: 0, marginBottom: 0}}
+    title='MORE INFO' />
+</Card>
+
+    <Text style={{ fontSize: 40, fontWeight: '300', paddingHorizontal: 80, color:'red' }}> Republicans </Text>
+    
+<Card
+  title='Donald Trump'
+  image={require('../voteSmart/images/dtrump.jpg')}>
+  <Button
+    buttonStyle={{borderRadius: 0, marginLeft: 0, marginRight: 0, marginBottom: 0}}
+    title='MORE INFO' />
+</Card>
+
+</View>
                 </ScrollView>
             </SafeAreaView>
         );
